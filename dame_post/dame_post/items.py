f7fb9dd894e75b573d4e1bd09f12782aae8eaffd
asce <asce@asce-SAM.(none)>
2013-06-20 03:57:08 +0200

    Scrapy no almacena items

---
+++ dame_post/dame_post/items.py
@@ -0,0 +1,23 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/topics/items.html
+
+from scrapy.item import Item, Field
+
+class DamePostItem(Item):
+    # define the fields for your item here like:
+    # name = Field()
+    title = Field()
+    author = Field()
+    content = Field()
+    category_list = Field()
+    tag_list = Field()
+
+"""
+    Título.
+Autor.
+Contenido.
+Lista de categorías.
+Lista de etiquetas
+"""
---
+++ dame_post/run.sh
@@ -0,0 +1,2 @@
+#/usr/bin/bash
+scrapy crawl dame_post -o post1.xml  -t xml
